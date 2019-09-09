106831269bea024521c19851c2c4f1b950151176
tanakorn0314 <tanakorn0412@gmail.com>
2019-09-09 13:44:56 +0700

    complete 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import CharacterCard from './CharacterCard';
 import './App.css';
+import _ from 'lodash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let message = 'Hello'
+
+const prepareStateFromWord = (given_word) => {
+  let word = given_word.toUpperCase()
+  let chars = _.shuffle(Array.from(word))
+  return {
+    word,
+    chars,
+    attempt: 1,
+    guess: [],
+    completed: false
+  }
+}
+
+class App extends React.Component {
+
+  state = prepareStateFromWord(message);
+
+  activationHandler = (c) => {
+    let guess = [...this.state.guess, c]
+    this.setState({ guess })
+    if (guess.length == this.state.chars.length) {
+      if (guess.join('').toString() == this.state.word) {
+        this.setState({ guess: [], completed: true })
+      } else {
+        this.setState({ guess: [], attempt: this.state.attempt + 1 })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          Array.from(this.state.chars).map((item, index) => (
+            <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+            />
+          ))
+        }
+        <h2>Selected</h2>
+        {
+          Array.from(this.state.guess).map((item, index) => (
+            <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+            />
+          ))
+        }
+        <div>Attemp {this.state.attempt}</div>
+        {
+          this.state.completed && <h4>Complete</h4>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
